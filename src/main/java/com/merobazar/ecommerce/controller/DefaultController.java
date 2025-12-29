1b921cc5e56d32525ae4d68933b554a2adc1d50a
Anush Shrestha <anush.stha980@outlook.com>
2025-12-29 13:42:46 +0545

    minor issue fixed

--- src/main/java/com/merobazar/ecommerce/controller/DefaultController.java
+++ src/main/java/com/merobazar/ecommerce/controller/DefaultController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DefaultController {
 
-    @GetMapping("/api/")
+    @GetMapping("/api/health")
     public String getString(){
         return "Welcome to MeroBazar Api.";
     }
